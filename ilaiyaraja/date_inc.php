f0d04c0e98a8ef32c7046dc9d59427908229edd1
dspriyann <dspriyan@dsp.local>
2014-02-11 15:55:59 +0530

    added code
    added set of data

--- ilaiyaraja/date_inc.php
+++ ilaiyaraja/date_inc.php
@@ -1,2 +1,22 @@
+<?php
+$start = '8:00';
+$time = strtotime($start);
+for($i=0;$i<20;$i++){
+
+$startTime = date("H:i", strtotime('-30 minutes', $time));
+$endTime = date("H:i", strtotime('+30 minutes', $time));
+echo "start-time: ".$startTime;
+echo "end-time: ".$endTime;
+echo "<br/>";
 $newTime = $startTime;
+
+if($startTime == $newTime){
 echo "start time is equal";
+}
+else{
+echo "end time perusu";
+}
+
+}
+
+?>
